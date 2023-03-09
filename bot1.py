929a98f400c7b21953cac29e8ab3f009bdd4edb2
andrey <andreilkt@yandex.ru>
2023-03-09 14:40:09 +0700

    09.03, добвлена вторая функция

--- bot1.py
+++ bot1.py
@@ -14,15 +14,27 @@ def start_handler(message):
 def text_handler(message):
     text = message.text.lower()
     chat_id = message.chat.id
-    if text == "привет" or text =="Hi":
+    if text == "привет":
         bot.send_message(chat_id, 'Привет, я бот - парсер скидок.')
+    else: bot.send_message(chat_id, 'Не Понял')
 
-    elif text == "какие" or text == "горячие":
-        bot.send_message(chat_id, 'Есть такие')
+@bot.message_handler(content_types=['text'])
+def text_handler1(message):
+    text1 = message.text.lower()
+    chat_id = message.chat.id
+    if text1 =="Hi":
+        bot.send_message(chat_id, 'Привет, я найду Вам выгодные скидки.')
 
 
-    elif text == "Огромные" or "Распродажи":
-        bot.send_message(chat_id, 'Таких много')
+    elif text1 == 'Скидки':
+        bot.send_message(chat_id, 'Какие')
+
+    elif text == 'горячие':
+        bot.send_message(chat_id, 'Сейчас найду')
+
+
+    elif text == 'Распродажи есть':
+        bot.send_message(chat_id, 'Растродажи есть')
 
 
     else:
